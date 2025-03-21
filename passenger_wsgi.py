9ebd21dfe1463338b50f89a75425d6418cfccf37
Emmattx <84171888+EmmattxDev@users.noreply.github.com>
2025-03-21 11:39:58 +0100

    made some update to the settings.py and passenegr_wsgi.py file to catch logs 2

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -4,10 +4,10 @@ import sys
 # Add the directory containing your Django project to the Python path
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-# If you're using a virtual environment, uncomment and modify these lines:
-INTERP = os.path.join(os.environ['HOME'], 'virtualenv/hazon/3.13/bin/python')
-if sys.executable != INTERP:
-    os.execl(INTERP, INTERP, *sys.argv)
+# # If you're using a virtual environment, uncomment and modify these lines:
+# INTERP = os.path.join(os.environ['HOME'], 'virtualenv/hazon/3.13/bin/python')
+# if sys.executable != INTERP:
+#     os.execl(INTERP, INTERP, *sys.argv)
 
 # Set the Django settings module (adjust if your settings are in a different location)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
